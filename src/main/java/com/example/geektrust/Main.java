f1a59810af3c1421ca3351dbdffcfd8193ebbba1
a-behera <ankitbehera799@gmail.com>
2023-06-20 11:57:47 +0530

    added command class as enum, modified main.java

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -1,20 +1,47 @@
-package com.example.geektrust; 
+package com.example.geektrust;
+
+import com.example.geektrust.common.Command;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Sample code to read from file passed as command line argument
         try {
+
             // the file to be opened for reading
-            FileInputStream fis = new FileInputStream(args[0]);
+            FileInputStream fis = new FileInputStream("sample_input\\input3.txt ");
+
             Scanner sc = new Scanner(fis); // file to be scanned
+
             // returns true if there is another line to read
             while (sc.hasNextLine()) {
+
                //Add your code here to process input commands
+                String input = sc.nextLine();
+                String[] command = input.split(" ");
+
+                if(Objects.equals(command[0], Command.ADD_DRIVER)){
+                    System.out.println();
+                } else if (Objects.equals(command[0], Command.ADD_RIDER)) {
+                    System.out.println();
+                } else if (Objects.equals(command[0], Command.MATCH)) {
+                    System.out.println();
+                } else if (Objects.equals(command[0], Command.START_RIDE)) {
+                    System.out.println();
+                } else if (Objects.equals(command[0], Command.STOP_RIDE)) {
+                    System.out.println();
+                } else if (Objects.equals(command[0], Command.BILL)) {
+                    System.out.println();
+                } else {
+                    System.out.println("Unknown Command");
+                }
             }
             sc.close(); // closes the scanner
         } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-        */
     }
 }
